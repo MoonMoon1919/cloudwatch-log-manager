4f5b4abf735e98174716fd28b596b21c5765b650
Max Moon <moon.maxwell@gmail.com>
2017-11-24 13:41:23 -0800

    Finishing up

--- dist/main.py
+++ dist/main.py
@@ -4,53 +4,74 @@ import json
 import os
 import sys
 
-logsClient = boto3.client('logs')
-expiration = 120
+EXPIRATION = 120
+REGIONS = ['us-west-1', 'us-west-2', 'us-east-1', 'us-east-2'] 
 
-def get_groups():
-    log_groups = []
+class RegionGroups(object):
+    def __init__(self, region):
+        self.region = region
 
-    try:
-        get_logs = logsClient.describe_log_groups()
+    def get_groups(self):
+        logsClient = boto3.client('logs', region_name=self.region)
 
-        if len(get_logs['logGroups']) != 0:
-            for group in get_logs['logGroups']:
-                log_groups.append(group['logGroupName'])
-    except ClientError as error:
-        print(error.response)
-    finally:
-        return log_groups
+        log_groups = []
 
-def get_retention_policy(groups, expiration):
-    groups_to_update = []
-
-    for group in groups:
         try:
-            log_group = logsClient.describe_log_groups(logGroupNamePrefix=group)
+            get_logs = logsClient.describe_log_groups()
 
-            group_info = log_group['logGroups'][0]
-
-            group_name = group_info['logGroupName']
-
-            if 'retentionInDays' in group_info and group_info['retentionInDays'] == expiration:
-                print('Log Group: "%s" passed check' % group_name.split('/')[3])
+            if len(get_logs['logGroups']) != 0:
+                for group in get_logs['logGroups']:
+                    log_groups.append(group['logGroupName'])
             else:
-                groups_to_update.append(group_info['logGroupName'])
-        except ClientError as error:
-            print(error.response)
-
-    return groups_to_update
-
-def put_group_policy(groups, expiration):
-    for group in groups:
-        try:
-            update_policy = logsClient.put_retention_policy(logGroupName=group, retentionInDays=expiration)
+                log_groups = None
         except ClientError as error:
             print(error.response)
         finally:
-            print('Updated groups %s with a policy of %s days' % (group, expiration))
-            
+            return log_groups
+
+    def get_retention_policy(self, groups, expiration):
+        logsClient = boto3.client('logs', region_name=self.region)
+
+        groups_to_update = []
+
+        for group in groups:
+            try:
+                log_group = logsClient.describe_log_groups(logGroupNamePrefix=group)
+
+                group_info = log_group['logGroups'][0]
+
+                group_name = group_info['logGroupName']
+
+                if 'retentionInDays' in group_info and group_info['retentionInDays'] == expiration:
+                    print('Log Group: "%s" passed check' % group_name.split('/')[3])
+                else:
+                    groups_to_update.append(group_info['logGroupName'])
+            except ClientError as error:
+                print(error.response)
+
+        return groups_to_update
+
+    def put_group_policy(self, groups, expiration):
+        logsClient = boto3.client('logs', region_name=self.region)
+
+        for group in groups:
+            try:
+                update_policy = logsClient.put_retention_policy(logGroupName=group, retentionInDays=expiration)
+            except ClientError as error:
+                print(error.response)
+            finally:
+                print('Updated groups %s with a policy of %s days' % (group, expiration))
+
+###### Entry #######
 def handler(event, context):
-    groups = get_groups()
-    groups_to_update = get_retention_policy(groups=groups, expiration=expiration)
-    update_groups = put_group_policy(groups=groups_to_update, expiration=expiration)
+    for region in REGIONS: 
+        print("Checking CloudWatch Logs in " + region)
+        region_groups = RegionGroups(region)
+
+        groups = region_groups.get_groups()
+
+        if groups != None:
+            groups_to_update = region_groups.get_retention_policy(groups=groups, expiration=EXPIRATION)
+            update_groups = region_groups.put_group_policy(groups=groups_to_update, expiration=EXPIRATION)
+        else:
+            print("No groups found")
