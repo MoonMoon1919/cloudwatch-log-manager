2a23d55151120faf8aa473ad4e11537bb0928c43
Max Moon <moon.maxwell@gmail.com>
2017-11-24 19:46:18 -0800

    fixing local_config potential values

--- dist/local_config.py
+++ dist/local_config.py
@@ -2,7 +2,7 @@ EXPIRATION = 120
 REGIONS = ['us-west-1', 'us-west-2', 'us-east-1', 'us-east-2'] 
 
 ## Expiration must be one of the following:
-# 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, and 365
+# 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, and 3653
 
 
 ## Regions must be a valid AWS region
